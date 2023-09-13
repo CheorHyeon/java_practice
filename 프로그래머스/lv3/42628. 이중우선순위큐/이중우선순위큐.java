68c98a7f6891551655b52c2f84225bd5c21d2b2b
cheorhyeon <126079049+CheorHyeon@users.noreply.github.com>
2023-09-13 14:57:03 +0900

    [level 3] Title: 이중우선순위큐, Time: 1.63 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Solution {
+	public int[] solution(String[] operations) {
+		// 1. 우선순위 큐 (min heap, max heap) 각각 생성
+		// 2. 명령어 실행하며 최소, 최대 삭제할 값을 최종 리스트에서 해당 값 제거
+		// 3. 최종 리스트 검사
+		List<Integer> resultList = new ArrayList<>();
+
+		for (String operation : operations) {
+			String[] operArr = operation.split(" ");
+
+			if (operArr[0].equals("I")) {
+				resultList.add(Integer.parseInt(operArr[1]));
+				Collections.sort(resultList);
+			} else {
+				// 없으면 무시하기 위함
+				if (resultList.size() > 0) {
+					Integer delTarget = null;
+					// 최솟값
+					if (operArr[1].startsWith("-")) {
+						delTarget = resultList.get(0);
+					}
+					// 최대값
+					else {
+						delTarget = resultList.get(resultList.size() - 1);
+					}
+					resultList.remove(delTarget);
+				}
+			}
+		}
+
+		int result[] = new int[2];
+
+		if (resultList.size() > 0) {
+			List<Integer> orderedResultList = resultList.stream()
+				.sorted(Comparator.reverseOrder())
+				.collect(Collectors.toList());
+			result[0] = orderedResultList.get(0);
+			result[1] = orderedResultList.get(resultList.size() - 1);
+		}
+
+		return result;
+	}
+}
