f133d0db64145efcccc778d661f2fa3153174f19
cheorhyeon <126079049+CheorHyeon@users.noreply.github.com>
2023-09-25 22:17:25 +0900

    [level 2] Title: 올바른 괄호, Time: 5.55 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,21 @@
 class Solution {
     boolean solution(String s) {
-        char[] tmp = s.toCharArray();
-        
+       char[] c = s.toCharArray();
         int result = 0;
-        for(int i=0; i<tmp.length; i++) {
-            if(tmp[i] == '(') {
+        
+        for(int i=0; i<c.length; i++) {
+            if(c[i] == '(')
                 result++;
-            }
-            else result--;
+            else
+                result--;
             
-            // 닫는거 먼저 나오거나 닫는게 더 많아지면 바로 false
             if(result < 0)
                 return false;
         }
         
-        // 다 돌았는데 결과가 0이 아니라는 것은 여는게 더 많은 경우 -> false
-        if(result != 0) {
+        if(result !=0)
             return false;
-        }
-
+        
         return true;
     }
 }
