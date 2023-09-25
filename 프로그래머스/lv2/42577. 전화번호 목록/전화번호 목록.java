48a0da10235e5f1bee7d52b445484444a7fdbf55
cheorhyeon <126079049+CheorHyeon@users.noreply.github.com>
2023-09-25 16:46:20 +0900

    [level 2] Title: 전화번호 목록, Time: 284.75 ms, Memory: 96.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,23 +1,13 @@
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
 	public boolean solution(String[] phone_book) {
-		// 1. HashMap을 만든다.
-		HashMap<String, Integer> map = new HashMap<>();
-		for(int i=0; i<phone_book.length; i++) {
-			map.put(phone_book[i], 1);
+		Arrays.sort(phone_book);
+		for(int i=1; i< phone_book.length; i++) {
+			if(phone_book[i].startsWith(phone_book[i-1]))
+				return false;
 		}
-		// 2. 모든 전화번호의 접두어가 HashMap에 있는지 확인한다.
-		for(int i=0; i< phone_book.length; i++) {
-			for(int j=1; j< phone_book[i].length(); j++) {
-				// 마지막 문자까지 substring을 하면 무조건 존재하니깐
-				// 접두사 까지인것 확인하기 위해 length-1까지만
-				if(map.containsKey(phone_book[i].substring(0, j))){
-					return false;
-				}
-			}
-		}
-		// 3. 여기까지 왔다면 접두어가 없다는 것
+		
 		return true;
 	}
 }
